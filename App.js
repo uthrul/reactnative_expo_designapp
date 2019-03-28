40cb166ea03fc4cccaf40fabb6c146decafab7e2
saud maruli <uthrul@gmail.com>
2019-03-28 14:00:00 +0700

    5. States and Animations

--- App.js
+++ App.js
@@ -1,110 +1,29 @@
 import React from "react";
-import { ScrollView, SafeAreaView } from "react-native";
-import styled from "styled-components";
-import Card from "./components/Card";
-import { Icon } from "expo";
-import { NotificationIcon } from "./components/Icons";
-import Logo from "./components/Logo";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Container>
-        <SafeAreaView>
-          <ScrollView style={{ height: "100%" }}>
-            <TitleBar>
-              <Avatar source={require("./assets/avatar.png")} />
-              <Title>Welcome back,</Title>
-              <Name>Saud maruli S</Name>
-              <NotificationIcon
-                style={{ position: "absolute", right: 20, top: 5 }}
-              />
-            </TitleBar>
-            <ScrollView
-              style={{
-                flexDirection: "row",
-                padding: 20,
-                paddingLeft: 12,
-                paddingTop: 30
-              }}
-              horizontal={true}
-            >
-              <Logo
-                image={require("./assets/logo-framerx.png")}
-                text="Framer X"
-              />
-              <Logo
-                image={require("./assets/logo-figma.png")}
-                text="Figma"
-              />
-            </ScrollView>
-            <Subtitle>Continue Learning</Subtitle>
-            <ScrollView
-              horizontal={true}
-              style={{ paddingBottom: 30 }}
-              showsHorizontalScrollIndicator={false}
-            >
-              <Card
-                title="Styled Components"
-                image={require("./assets/path196.png")}
-                caption="React Native"
-                logo={require("./assets/logo-react.png")}
-                subtitle="5 of 12 sections"
-              />
-              <Card
-                title="Styled Components 2"
-                image={require("./assets/path196.png")}
-                caption="React Native"
-                logo={require("./assets/logo-react.png")}
-                subtitle="5 of 12 sections"
-              />
-            </ScrollView>
-          </ScrollView>
-        </SafeAreaView>
-      </Container>
-    );
+const initialState = {
+  action: ""
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return { action: "openMenu" };
+    case "CLOSE_MENU":
+      return { action: "closeMenu" };
+    default:
+      return state;
   }
-}
+};
 
-const Subtitle = styled.Text`
-  color: #b8bece;
-  font-weight: 600;
-  font-size: 15px;
-  margin-left: 20px;
-  margin-top: 10px;
-  text-transform: uppercase;
-`;
+const store = createStore(reducer);
 
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+);
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #f0f3f5;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  color: #b8bece;
-  font-weight: 500;
-`;
-
-const Name = styled.Text`
-  font-size: 20px;
-  color: #3c4560;
-  font-weight: bold;
-`;
-
-const TitleBar = styled.View`
-  width: 100%;
-  margin-top: 50px;
-  padding-left: 80px;
-`;
+export default App;
